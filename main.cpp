0878e661fa018b8c76e3837ea235f3476f66594e
Marat Saitov <maratsajtov@gmail.com>
2023-10-02 23:52:35 +0300

    2nd commit - Все же выдает неправильные значения но уже ближе к верным. Предположение, что проблема в хранении памяти в c++

--- main.cpp
+++ main.cpp
@@ -1,37 +1,42 @@
 #include <iostream>
-#include <math.h> 
-#define E 0.000001
+#include <math.h>
 
 double Qn(double n, double x){
     return -(((x * x)/(n+1)) * ((2*n+1)/(2*n+3)));
 }
 int main(){
-    double eps = E;
+    double eps = pow(10, -6);
 
-    double sum[10];
+    double sum[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     int sumInd = 0;
-    double x = 0;
+    double x = 0.0;
+
     while(x <= 2){
+        std::cout << "x" << sumInd << " = " << x << "\n\n";
+        double n = 0;
         double prevA = x;
         sum[sumInd] += prevA;
+        
+        std::cout << "n = " << n << "\n";
+        std::cout << "an = " << prevA << "\n\n";
 
-        double n = 1;
-
-        std::cout << "x = " << x << "\n" /*<< "Qn = " << Qn(n, x) << "\n"*/;
+        n++;
+        
 
         while(prevA >= eps){
-            std::cout << "При n = " << n << "\n";
-            std::cout << "Модуль Q[" << n-1 << "] = " << abs(Qn(n-1, x)) <<"\n";
-            std::cout << "\n";
-
-            prevA = abs(Qn(n-1, x) * prevA);
+            std::cout << "n = " << n << "\n";
+            //std::cout << "Q" << n-1 << " = " << abs(Qn(n-1, x)) <<"\n";
+            
+            prevA = Qn(n-1, x) * prevA;
             sum[sumInd] += prevA;
             
-            //std::cout << prevA << "\n";
+            std::cout << "an = " << prevA << "\n";
+            std::cout << "\n";
             n++;
         }
-        std::cout << "Прошелся по циклу " << n << " раз"<< "\n";
-        std::cout << "сумма под индексом " << sumInd << " = " << sum[sumInd] << "\n";
+        sum[sumInd] *= 2/sqrt(M_PI);
+        std::cout << "Кол-во итераций - " << n << "\n";
+        std::cout << "F(x" << sumInd << ") = " << sum[sumInd] << "\n";
         std::cout << "------------------------------------------"<< "\n"; 
         x += 0.2;
         sumInd++;
